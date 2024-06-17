90459e85efa80a68af1a3c5e34ab4ed6aa0465d2
Lucas Konstantin Bärenfänger <18075314+lkbaerenfaenger@users.noreply.github.com>
2024-06-17 12:31:04 +0000

    Remove ESCB login button

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -15,7 +15,7 @@ const LoginForm = ({ login }) => {
 
   return (
     <div className='card'>
-      <h6 className='card-header text-center p-3'>Login</h6>
+      <h6 className='card-header text-center p-3'>Login to proceed</h6>
       <div className='card-body'>
         <form onSubmit={handleFormSubmit}>
           <div className='form-floating mb-3'>
@@ -43,11 +43,8 @@ const LoginForm = ({ login }) => {
             <label htmlFor='floatingPassword'>Password</label>
           </div>
           <div className='d-grid'>
-            <button className='btn btn-primary mb-3' type='submit'>
+            <button className='btn btn-primary' type='submit'>
               Login
-            </button>
-            <button className='btn btn-primary' type='submit' disabled>
-              Login via ESCB IAM
             </button>
           </div>
         </form>
